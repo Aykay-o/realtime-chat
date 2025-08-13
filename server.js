705440551bef66d3a548cce678a6debabeffae5f
Aykay-o <126713029+Aykay-o@users.noreply.github.com>
2025-08-13 23:57:55 +0100

    setup index.html and simple socket.io connection

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import http from 'http';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path'; 
+import { Server } from 'socket.io';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express(); // will handle normal webpage + api routes
+const server = http.createServer(app); // we create the server explicitely
+const io = new Server(server); // will handle websocket stuff
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("user has entered the server:", socket.id);
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
